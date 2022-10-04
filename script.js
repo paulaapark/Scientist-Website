4815520f2fb17d7ef145ce628d9946ade52b349a
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-10-04 14:01:39 -0400

    Failed attempt to button

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-function style_me(){
-    document.getElementsByClassName("container").setAttribute("class", "style");
+function style_me() {
+    document.getElementsByClassName('container').setAttribute("class", "style");
 
 }
 
 
 
-function add(){
-    document.getElementsByClassName("text-input").value = document.getElementById("textarea").value;
+function add_scientist() {
+    document.getElementsByClassName('text-input').value = document.getElementById('textarea').value;
 }
