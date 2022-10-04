15bc1f86e4497ed8371325ff794e808a2406bd5a
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-10-04 13:39:39 -0400

    Attempting onclick functions

---
+++ script.js
@@ -0,0 +1,10 @@
+function style_me(){
+    document.getElementsByClassName("container").setAttribute("class", "style");
+
+}
+
+
+
+function add(){
+    document.getElementsByClassName("text-input").value = document.getElementById("textarea").value;
+}
