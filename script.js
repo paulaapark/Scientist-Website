a338d9b14afdb194f1413656ff42270bf1473eed
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-10-04 16:46:17 -0400

    Failed button attempt

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 
-var element = document.getElementsByClassName("container");
-
-var elementa = document.getElementsByClassName("event-name");
-
-var elementb = document.getElementsByClassName("event-info");
-
-var elementc = document.getElementById("register");
-
-
-
 
 function style_me(){
-    element.classList.toggle("style0");
-    elementa.classList.toggle("style1");
-    elementb.classList.toggle("style2");
-    elementc.classList.toggle("style3");
+    if ( document.getElementById("events").classList.contains('container' 'event-name' 'event-info' 'register');
+    document.getElementsByClassName("container" "event-name" "event-info" "register").classList.toggle('style0' 'style1' 'style2' 'style3');
+    // document.getElementsByClassName("event-name").classList.toggle("style1");
+    // document.getElementsByClassName("event-info").classList.toggle("style2");
+    // document.getElementById("register").classList.toggle("style3");
 }
+
+// document.getElementById("MyElement").classList.toggle('MyClass');
 
 
 
 function add_scientist() {
-    document.getElementsByClassName('text-input').value = document.getElementById('textarea').value;
+    document.getElementbyId("box").value = document.getElementById("text-input").value;
 }
