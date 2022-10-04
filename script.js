e13982216829b5c24eb536c4dbd8a7563e7f0d12
paulaapark <99138656+paulaapark@users.noreply.github.com>
2022-10-04 15:50:57 -0400

    Re-attempting the buttons

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function style_me() {
-    document.getElementsByClassName('container').setAttribute("class", "style");
 
+var element = document.getElementsByClassName("container");
+
+var elementa = document.getElementsByClassName("event-name");
+
+var elementb = document.getElementsByClassName("event-info");
+
+var elementc = document.getElementById("register");
+
+
+
+
+function style_me(){
+    element.classList.toggle("style0");
+    elementa.classList.toggle("style1");
+    elementb.classList.toggle("style2");
+    elementc.classList.toggle("style3");
 }
 
 
